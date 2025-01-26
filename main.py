61f77c5110b8b30e30e928d93344d108b04b9f71
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2025-01-26 20:21:43 +0100

    Updating board

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 def game():
+    #creating a board
+    st_row = [1,2,3]
+    nd_row = [4,5,6]
+    rd_row = [7,8,9]
+    board =[st_row,nd_row,rd_row]
+    win = True
     #endless loop for a game
-    while 1>0:
-        #creating a board
-        st_row = [1,2,3]
-        nd_row = [4,5,6]
-        rd_row = [7,8,9]
-        board =[st_row,nd_row,rd_row]
-
-        #printin out a board
+    while win:
+        #printing out a board
         for x in board:
             print (x)
-            
+
         #taking input from players
         player_one = int(input("Player1 - Enter where would you like to put O:"))
         player_two = int(input("Player2 - Enter where would you like to put X:"))
         diff = True
         while diff:
-            #create better checking for input from user
+            #!!!CREATE BETTER CHECKING FOR INPUT FROM USER!!!
             if(player_one!=player_two and 0<player_one<10 and 0<player_two<10):
                 diff = False
             else:
                 player_two = int(input("Player2 - Enter again where would you like to put X:"))
-        break
+        
+        charackter = "O"
+        #checking where to put O and X
+        #!!!CREATE CHECKING IF CHARACKTER IS ALREADY THERE!!!
+        for x in [player_one, player_two]:
+            if(x<4):
+                st_row[x-1] = charackter
+            elif(x<7):
+                nd_row[x-4] = charackter
+            else:
+                rd_row[x-7] = charackter
+            charackter = "X"
 
 game()
