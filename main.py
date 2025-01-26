b48565286a3983771a272570557946b8b1147c5a
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2025-01-26 19:00:08 +0100

    Creating board

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def boardgame():
-    print("board")
+def game():
+    st_row = [1,2,3]
+    nd_row = [4,5,6]
+    rd_row = [7,8,9]
+    board =[st_row,nd_row,rd_row]
+    for x in board:
+        print (x)
+    #val = input("Enter where would you like to put your mark:")
 
-boardgame()
+game()
