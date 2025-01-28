db9c8caccf1dc9dd2f34b04cdf021c0be03f8711
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2025-01-28 18:09:10 +0100

    Improving code 1.3

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import sys, os
 
 #printing out a board
 def printing_out_board():
+    #clearing terminal for better visual
     os.system('cls')
     for x in board:
         print (x)
 
-#checking where to put O and X
+#updating board
 def updating_board(where_to_put_mark, which_player_move):
+        #define which player has moved
         if(which_player_move==1):
             charackter = "O"
         else: 
             charackter = "X"
 
+        #defing where to put charackter on the board
         if(where_to_put_mark<4):
             st_row[where_to_put_mark-1] = charackter
         elif(where_to_put_mark<7):
@@ -21,18 +24,18 @@ def updating_board(where_to_put_mark, which_player_move):
             rd_row[where_to_put_mark-7] = charackter
 
 #function checking if game has ended
-#return true if game has ended and number of player who won
-#!!! CREATE BETTER CHECKING IF SOMEONE WON
-#!!! WHAT IF LIST WILL HAVE SOMETHING LIKE (1,4,2,3)
+#return true if game has ended
 def check_win(moves_left, player_moves):
-    #condition to win a game
+    #conditions to win a game
     cond =  ([1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7])
 
     if (moves_left<=4):
         for x in cond:
+            #verifying is player list of moves has numbers from conditions
             if(all(number in player_moves for number in x)):
                 return True
-            
+
+    #handling game ending in draw 
     elif(moves_left==0):    
         return True
     else:
@@ -40,6 +43,7 @@ def check_win(moves_left, player_moves):
 
 #printing out who won
 def is_game_over(moves_left, player_moves, which_player):
+    #checking is game over
     is_game_ended = check_win(moves_left, player_moves)
     if (is_game_ended):
         printing_out_board()
@@ -50,7 +54,7 @@ def is_game_over(moves_left, player_moves, which_player):
         print("It's a draw")
         sys.exit()
     else:
-        print('')
+        return None
 
 def game():
     #creating a board
@@ -70,22 +74,19 @@ def game():
     #endless loop for a game
     while True:
         printing_out_board()
-
         #taking input from players
         player_one = int(input("Player1 - Enter where would you like to put O:"))
         #!!!TO DO checking input from the player
         #keep track of player moves
         st_player_moves.append(player_one)
-
         updating_board(player_one, 1)
         m-=1
         is_game_over(m, st_player_moves, 1)
-        printing_out_board()
 
+        printing_out_board()
         player_two = int(input("Player2 - Enter where would you like to put X:"))
         #!!!TO DO checking input from the player
         nd_player_moves.append(player_two)
-
         updating_board(player_two, 2)
         m-=1
         is_game_over(m, nd_player_moves, 2)
@@ -101,9 +102,6 @@ def game():
         #        player_two = int(input("Player2 - Enter again where would you like to put X:"))
 
 game()
-
-#function for a computer player
-#def computer():
 
 #implementing minmax algorithm
 def minimax(moves_left, first_player_moves, second_player_moves):
