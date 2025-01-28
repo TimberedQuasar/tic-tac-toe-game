b773f355706c1384d876f271e6b4eec292bbb4f3
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2025-01-28 13:15:33 +0100

    Spliting code into functions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys, os
+
+#creating a board
+st_row = [1,2,3]
+nd_row = [4,5,6]
+rd_row = [7,8,9]
+board =[st_row,nd_row,rd_row]
+
 def game():
-    #creating a board
-    st_row = [1,2,3]
-    nd_row = [4,5,6]
-    rd_row = [7,8,9]
-    board =[st_row,nd_row,rd_row]
 
     #keep track of players moves
     st_player_moves = []
@@ -12,9 +14,6 @@ def game():
 
     #keep track of rounds
     r = 0
-
-    #condition to win a game
-    cond =  ([1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7])
 
     #endless loop for a game
     win = True
@@ -27,9 +26,11 @@ def game():
         player_one = int(input("Player1 - Enter where would you like to put O:"))
         player_two = int(input("Player2 - Enter where would you like to put X:"))
 
+        #handling input from user
+        #!!!CREATE CHECKING IF CHARACKTER IS ALREADY THERE!!!
+        #!!!CREATE BETTER CHECKING FOR INPUT FROM USER!!!
         diff = True
         while diff:
-            #!!!CREATE BETTER CHECKING FOR INPUT FROM USER!!!
             if(player_one!=player_two and 0<player_one<10 and 0<player_two<10):
                 diff = False
             else:
@@ -38,36 +39,16 @@ def game():
         st_player_moves.append(player_one)
         nd_player_moves.append(player_two)
 
-        #checking where to put O and X
-        #!!!CREATE CHECKING IF CHARACKTER IS ALREADY THERE!!!
-        charackter = "O"
-        for x in [player_one, player_two]:
-            if(x<4):
-                st_row[x-1] = charackter
-            elif(x<7):
-                nd_row[x-4] = charackter
-            else:
-                rd_row[x-7] = charackter
-            charackter = "X"
         
         #clearing terminal for better visual
         os.system('cls')
         #checking if someone won
         r+=1
-        if (r>=3):
-            st_player_moves.sort()
-            nd_player_moves.sort()
-            i=0
-            for x in [st_player_moves, nd_player_moves]:
-                i+=1
-                for y in cond:
-                    if x == y:
-                        print("Player"+str(i)+ " won")
-                        sys.exit()
-            
-            print("It's a draw")
-            sys.exit()
+        check_win(r, st_player_moves, nd_player_moves)
 
+game()
+
+#function for a computer player
 def computer():
     #implementing minmax algorithm
     is_game_won = False
@@ -105,6 +86,33 @@ def computer():
                 value = min(value, minimax())
             return value
 
+#checking where to put O and X
+def updating_board(player_one, player_two):
+        charackter = "O"
+        for x in [player_one, player_two]:
+            if(x<4):
+                st_row[x-1] = charackter
+            elif(x<7):
+                nd_row[x-4] = charackter
+            else:
+                rd_row[x-7] = charackter
+            charackter = "X"
 
-
-game()
+#function checking if game has ended
+#!!!CREATE BETTER CHECKING FOR GAME ENDING IN DRAW
+def check_win(rounds, first_player_moves, second_player_moves):
+    #condition to win a game
+    cond =  ([1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7])
+    if (rounds>=3):
+        first_player_moves.sort()
+        second_player_moves.sort()
+        for x in [first_player_moves, second_player_moves]:
+            i=1
+            for y in cond:
+                if x == y:
+                    print("Player"+str(i)+ " won")
+                    sys.exit()
+            i+=1
+        if(rounds>=4):    
+            print("It's a draw")
+            sys.exit()
